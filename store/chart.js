bedb4befc623dc470651cc7a842ad32406e85b50
HungNhOnPV <anhhungx9propro@gmail.com>
2021-06-01 22:58:31 +0700

    map api chart-holder

--- store/chart.js
+++ store/chart.js
@@ -7,7 +7,6 @@ export const getters = {}
 export const actions = {
   getHolder: ({ commit }, { contractAddress, timeLine }) => {
     return axios.get(`${process.env.NUXT_ENV_API}/api/v1/coin/getHolders?contractAddress=${contractAddress}&timeLine=${timeLine}`)
-    // return axios.get(`https://demo-live-data.highcharts.com/aapl-c.json`)
   },
 }
 
